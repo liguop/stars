a2af99b16f5e3bd5bdb0e1383a30bb5ba7370f98
lgp <gp1907971839@163.com>
2018-07-20 18:12:04 +0800

    加点注释

--- stars1.java
+++ stars1.java
@@ -1,21 +1,24 @@
-public class starts1{
+public class wan{
     public static void main(String[]args){
         stars(6);
     }
     static void stars(int r){
         int row=1,xx=0; //row是打印多少行， xx是控制是否打星星
     bb:
-        for(int lie=1,kong=r;lie<=r*2-1;lie++){
-            if(kong-lie>0||xx>=row*2-1){
+        // column是多少列（星星+空格的个数），space是控制空格的数量
+        for(int column=1,space=r;column<=r*2-1;column++){
+            if(space-column>0||xx>=row*2-1){
                 System.out.print(" ");
             }else{
                 System.out.print("*"); 
                 xx++; 
             }
-            if(row<r&&lie==r*2-1){
+            // 打印完一列后，continue 到标签，继续重新打印
+            if(row<r&&column==r*2-1){
                 System.out.println();
-                kong--;row++;
-                xx=0;lie=0;
+                space--;row++;
+                //xx和column需要重新计数
+                xx=0;column=0;
                 continue bb;
             }
         }
